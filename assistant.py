bc2ace116a63ee89c36d19f993868c6e7764febe
Dodla Nikhil <nikhildodla276@gmail.com>
2026-03-27 18:15:26 +0000

    feat: add imports and projects structure to assistant.py

--- assistant.py
+++ assistant.py
@@ -1,9 +1,14 @@
-# Exam Assistant - Project 2
-# Author: Nikhil Dodla
-# Purpose: Read PDF study material and generate prioritized exam notes
+"""
+Exam Assistant - Project 2
+Author: Nikhil Dodla
+Purpose: Read PDF study material notes and generates priotrized exam notes
+"""
 
-# Step 1: Accept PDF file from user
-# Step 2: Extract text from PDF using PyMuPDF (fitz)
-# Step 3: Send extracted text to Groq LLM API
-# Step 4: Receive prioritized exam notes from LLM
-# Step 5: Display notes to user
+# Standard libraries
+import sys
+
+# Third-Party libraries
+import fitz
+from dotenv import load_dotenv
+
+load_dotenv()
